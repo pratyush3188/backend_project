760febfb1346b3fefdeadbb825b01ea88bb5549b
PratyushSharma <psvt2007@gmail.com>
2025-11-02 16:47:13 +0530

    app

--- src/app.js
+++ src/app.js
@@ -10,17 +10,22 @@ app.use(cors({
 }))
 
 app.use(express.json({
-    limit:'16kb '
+    limit:'50mb '
 }))
 app.use(express.urlencoded({
     extended:true,
-    limit:'16kb'
+    limit:'50mb'
 
 }))
 
 app.use(express.static("public"))
 
 app.use(cookieParser())
+// routes importing 
+import userRouter from './routes/user.routes.js'
 
+
+// routes declaration 
+app.use('/api/v1/users',userRouter)
 
 export {app}
