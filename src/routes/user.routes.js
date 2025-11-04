b305038a16b21bcab32c85ae3a3f0788c615daef
PratyushSharma <psvt2007@gmail.com>
2025-11-04 15:14:47 +0530

    routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import express from "express";
+import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
-const router = express.Router();
+const router = Router()
 
-router.post("/register", registerUser);
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverimage",
+            maxCount:1
+        }
+
+    ]),
+    registerUser)
 
 export default router;
